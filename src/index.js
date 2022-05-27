3952ace7bd468c609387acda5b89f1a44ee7e8f3
Jagger <634750802@qq.com>
2022-05-27 10:49:39 +0800

    add health check

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ const Koa = require('koa')
 const logger = require('koa-logger')
 const { getUsername } = require('./accounts-client')
 const proxyAsktug = require('./asktug-proxy')
+const { asktug } = require('./config')
+const Axios = require("axios");
 
 const app = new Koa()
+
+app.use(async (ctx, next) => {
+  if (ctx.path === '/healthy/live') {
+    ctx.response.statusCode = 200
+    ctx.response.body = 'OK'
+  } else {
+    await next()
+  }
+})
 
 app.use(logger())
 
@@ -12,6 +23,21 @@ if (Boolean(process.env.ENABLE_CORS)) {
   app.use(cors({ credentials: true }));
   console.log('[cors] enabled')
 }
+
+app.use(async (ctx, next) => {
+  if (ctx.request.path === '/healthy/ready') {
+    try {
+      await Axios.head(asktug.url, {headers: {accept: 'application/json'}})
+      ctx.response.statusCode = 200
+      ctx.response.body = 'OK'
+    } catch (e) {
+      ctx.response.statusCode = 502
+      ctx.response.body = `failed to HEAD ${asktug.url}, ${String(e)}`
+    }
+  } else {
+    await next()
+  }
+})
 
 app.use(async (ctx, next) => {
   try {
@@ -28,7 +54,7 @@ app.use(async ctx => {
     const username = await getUsername(ctx)
     await proxyAsktug(ctx, username)
   } catch (e) {
-    console.error(e)
+    console.error('proxy failed', e)
     ctx.status = e?.response?.status ?? e?.status ?? 500
   }
 })
